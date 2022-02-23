f30a3336d561b7d6c41ccd9a4aedb1b373695f0f
Sonia Akhter <sonia798524@gmail.com>
2022-02-23 12:10:21 +0600

    restapi_test

---
+++ report.js
@@ -0,0 +1,17 @@
+const newman = require('newman');
+
+newman.run({
+    collection: require('./collection/user_restapi_collection.json'), // pointing to local JSON file.
+    environment: require('./collection/user_restapi_env.json'), // pointing to local env file
+    iterationCount: 1,
+    reporters: 'htmlextra',
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+    
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
